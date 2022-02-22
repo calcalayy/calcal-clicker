a26d5e7517ac3db4a50d7fdbbb2d3b5a354d2865
calcalayy <minelandsmc14@gmail.com>
2022-02-22 16:52:52 +0000

    initial commiot

---
+++ autoclicker.py
@@ -0,0 +1,45 @@
+import time
+import tkinter as tk
+import pyautogui as pag
+
+top = tk.Tk()
+
+#sort out stuff like title n stuff and resize
+top.geometry('180x200')
+top.resizable(False, False)
+top.title('ac')
+#text
+l = tk.Label(top, text = "Input clicks")
+l.config(font =("Courier", 8))
+l.place(x=90, y=15)
+
+#clcik stuff
+current_value = tk.StringVar()
+a = tk.Spinbox(top, from_=0, to=100, textvariable=current_value)
+a.place(x=95, y = 0)
+
+#very important this make fastt
+pag.PAUSE = 0
+
+#start the impotatnt
+def startClick():
+   time.sleep(3)
+   for x in range (int(current_value.get())):
+       pag.click()
+
+#buttinsss
+w = tk.Button(top, text= "click", command=startClick)
+w.place(x=75, y=100)
+
+#text
+l = tk.Label(top, text = "Start clicking.")
+l.config(font =("Courier", 8))
+l.place(x=35, y=125)
+
+#text aghain
+l = tk.Label(top, text = "(3 Second Delay)")
+l.config(font =("Courier", 8))
+l.place(x=30, y=150)
+
+#end of gui
+top.mainloop()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# calcal-clicker
+my autoclicker
+
+# how to install
+install and run
+
+# features
+trash ui made by me
+clicks like 1000 cps
+
+# soon
+customise speed
+
+# find bugs?
+open an issue
